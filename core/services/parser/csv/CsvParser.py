17e455bc1286415560f24f806fe2439f6d91ee15
qwrttqr <alekseenkopetr25@gmail.com>
2026-09-16 18:32:29 +0500

    init

---
+++ core/services/parser/csv/CsvParser.py
@@ -0,0 +1,38 @@
+import io
+import pandas as pd
+
+from fastapi import UploadFile
+
+
+class CsvParser:
+    @staticmethod
+    async def parse_from_network(files: list[UploadFile]) -> pd.DataFrame:
+        df_list = []
+
+        for file in files:
+            contents = await file.read()
+
+            parsed_df = pd.read_csv(io.BytesIO(contents), sep=';', encoding='windows-1251')
+
+            df_list.append(parsed_df)
+
+            await file.close()
+
+        if df_list:
+            return pd.concat(df_list, ignore_index=True)
+
+        return pd.DataFrame()
+
+    @staticmethod
+    async def parse_from_disk(file_paths: list[str]) -> pd.DataFrame:
+        df_list = []
+
+        for file_path in file_paths:
+            parsed_df = pd.read_csv(file_path, sep=';', encoding='windows-1251')
+
+            df_list.append(parsed_df)
+
+        if df_list:
+            return pd.concat(df_list, ignore_index=True)
+
+        return pd.DataFrame()
